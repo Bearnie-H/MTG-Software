478305e8143615325109c76b5ac486d8f204ce66
Joseph Sadden <56144991+Bearnie-H@users.noreply.github.com>
2025-08-05 10:35:28 -0700

    Adding stubs for cortical explant analysis.

---
+++ Python/MTG_Common/CorticalExplantQuantification.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+#   Author: Joseph Sadden
+#   Date:   31st July, 2025
+
+#   Script Purpose: ...
+#                       ...
+
+#   Import the necessary standard library modules
+from __future__ import annotations
+import typing
+#   ...
+
+#   Import the necessary third-part modules
+#   ...
+
+#   Import the desired locally written modules
+#   ...
+
+class StatusCode(int):
+    """
+    StatusCode
+
+    This class...
+    """
+
+class CorticalExplantConfiguration:
+    """
+    CorticalExplantConfiguration:
+
+    ...
+    """
+
+    ##  Magic Methods
+    def __init__(self) -> None:
+        """
+        Constructor:
+
+        ...
+        """
+        return
+
+    #   ...
+
+    ##  Public Methods
+    #   ...
+
+    ##  Private Methods
+    #   ...
+
+class CorticalExplantQuantificationResults():
+    """
+    CorticalExplantQuantificationResults
+
+    This class...
+    """
+
+    def __init__(self: CorticalExplantQuantificationResults) -> None:
+        """
+        Constructor
+
+        This function...
+
+        Return (None):
+            ...
+        """
+
+        return
